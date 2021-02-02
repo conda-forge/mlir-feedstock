e5a94ea6c58de8c81625996bcc98c72797543188
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-02-02 13:00:00 -0600

    adds cmake install prefix; tests for existence of mlir-opt

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@ mkdir build
 cd build
 
 cmake ${CMAKE_ARGS} \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_PREFIX_PATH="${PREFIX}" \
   -DLLVM_BUILD_LLVM_DYLIB=ON \
   -DLLVM_BUILD_LLVM_DYLIB=ON \
   ../mlir

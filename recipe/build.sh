0d5b8ef632d6a90261564becc175ecbb73d2be24
Uwe L. Korn <uwelk@xhochy.com>
2021-03-28 21:24:43 +0200

    Use LLVM_NATIVE cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,37 +2,22 @@
 
 set -euxo pipefail
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  (
-    mkdir -p build-host
-    pushd build-host
-
-    export CC=$CC_FOR_BUILD
-    export CXX=$CXX_FOR_BUILD
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-
-    # Unset them as we're ok with builds that are either slow or non-portable
-    unset CFLAGS
-    unset CXXFLAGS
-
-    cmake .. \
-      -DCMAKE_PREFIX_PATH=${BUILD_PREFIX} \
-      -DLLVM_DIR=${BUILD_PREFIX} \
-      ../mlir
-    make -j${CPU_COUNT} mlir-tblgen mlir-linalg-ods-gen
-    cp bin/mlir-linalg-ods-gen bin/mlir-tblgen ${BUILD_PREFIX}/bin
-    popd
-  )
-  CMAKE_ARGS="$CMAKE_ARGS -DMLIR_TABLEGEN=$SRC_DIR/build-host/bin/mlir-tblgen"
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  CMAKE_ARGS="${CMAKE_ARGS} -DLLVM_TABLEGEN_EXE=$BUILD_PREFIX/bin/llvm-tblgen -DNATIVE_LLVM_DIR=$BUILD_PREFIX/lib/cmake/llvm"
+  CMAKE_ARGS="${CMAKE_ARGS} -DCROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_C_COMPILER=$CC_FOR_BUILD;-DCMAKE_CXX_COMPILER=$CXX_FOR_BUILD;-DCMAKE_C_FLAGS=-O2;-DCMAKE_CXX_FLAGS=-O2;-DCMAKE_EXE_LINKER_FLAGS=;-DCMAKE_MODULE_LINKER_FLAGS=;-DCMAKE_SHARED_LINKER_FLAGS=;-DCMAKE_STATIC_LINKER_FLAGS=;-DCMAKE_AR=$BUILD_PREFIX/bin/${AR};-DCMAKE_RANLIB=${BUILD_PREFIX}/bin/${RANLIB}"
+else
+  rm -rf $BUILD_PREFIX/bin/llvm-tblgen
 fi
 
 mkdir -p build
 cd build
 
 cmake ${CMAKE_ARGS} \
+  -DLLVM_USE_HOST_TOOLS=1 \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_BUILD_LLVM_DYLIB=ON \
+  -DLLVM_LINK_LLVM_DYLIB=ON \
   -DLLVM_BUILD_TOOLS=ON \
   ../mlir
 

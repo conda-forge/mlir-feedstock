280bc672bc90677bc97f253943dfae965d14db20
Uwe L. Korn <uwelk@xhochy.com>
2021-03-29 07:37:56 +0200

    Remove obsolete sections

--- recipe/install_libmlir.sh
+++ recipe/install_libmlir.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -x -e
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  pushd build-host
-    cp bin/mlir-linalg-ods-gen bin/mlir-tblgen ${BUILD_PREFIX}/bin
-  popd
-fi
-
 cd ${SRC_DIR}/build
 make install
 
--- recipe/install_mlir.sh
+++ recipe/install_mlir.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == 1 ]]; then
-  pushd build-host
-    cp bin/mlir-linalg-ods-gen bin/mlir-tblgen ${BUILD_PREFIX}/bin
-  popd
-fi
-
 cd build
 make install

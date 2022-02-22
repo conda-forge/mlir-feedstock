7951eaa8298836e885d084175f2a5ad5e0e131dc
Uwe L. Korn <uwe.korn@quantco.com>
2022-02-22 15:39:14 +0100

    Helper libs are no longer installed

--- recipe/install_libmlir.sh
+++ recipe/install_libmlir.sh
@@ -11,14 +11,8 @@ mkdir lib
 cp lib2/libLLVM* lib/
 if [[ "$PKG_NAME" == "libmlir" ]]; then
     cp lib2/libMLIR${SHLIB_EXT} lib/
-    cp lib2/libmlir_runner_utils${SHLIB_EXT} lib/
-    cp lib2/libmlir_c_runner_utils${SHLIB_EXT} lib/
-    cp lib2/libmlir_async_runtime${SHLIB_EXT} lib/
 else
     cp lib2/libMLIR.*.* lib/
-    cp lib2/libmlir_runner_utils.*.* lib/
-    cp lib2/libmlir_c_runner_utils.*.* lib/
-    cp lib2/libmlir_async_runtime.*.* lib/
 fi
 rm -rf lib2
 

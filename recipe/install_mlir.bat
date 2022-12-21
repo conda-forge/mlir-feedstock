fc559f639533b9341664ec65f87c9150378021de
Jim Kitchen <jim22k@gmail.com>
2022-12-21 14:50:56 -0600

    Update install_mlir.bat with correct action for mlir-python-bindings

--- recipe/install_mlir.bat
+++ recipe/install_mlir.bat
@@ -1,7 +1,10 @@
 cd build
 ninja install
 
-if "%PKG_NAME%" == "mlir" (
-    rmdir /s /q "%PREFIX%"\\python_packages
-    rmdir /s /q "%PREFIX%"\\src
+if "%PKG_NAME%" == "mlir-python-bindings" (
+    if not exist "%SP_DIR%" mkdir "%SP_DIR%"
+    move "%PREFIX%"\\python_packages\\mlir_core\\mlir "%SP_DIR%"\\
 )
+
+rmdir /s /q "%PREFIX%"\\python_packages
+rmdir /s /q "%PREFIX%"\\src

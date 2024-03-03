d461d87ae2a5d0353c21c15bd4f7fb2830369452
H. Vetinari <h.vetinari@gmx.com>
2024-03-03 16:22:13 +1100

    also copy libmlir_float16_utils to libmlir* outputs

--- recipe/install_libmlir.sh
+++ recipe/install_libmlir.sh
@@ -7,14 +7,16 @@ cmake --install ./build --prefix=./temp_prefix
 
 if [[ "$PKG_NAME" == "libmlir" ]]; then
     cp ./temp_prefix/lib/libMLIR${SHLIB_EXT} $PREFIX/lib/
-    cp ./temp_prefix/lib/libmlir_runner_utils${SHLIB_EXT} $PREFIX/lib/
-    cp ./temp_prefix/lib/libmlir_c_runner_utils${SHLIB_EXT} $PREFIX/lib/
     cp ./temp_prefix/lib/libmlir_async_runtime${SHLIB_EXT} $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_c_runner_utils${SHLIB_EXT} $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_float16_utils${SHLIB_EXT} $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_runner_utils${SHLIB_EXT} $PREFIX/lib/
 else
     cp ./temp_prefix/lib/libMLIR.*.* $PREFIX/lib/
-    cp ./temp_prefix/lib/libmlir_runner_utils.*.* $PREFIX/lib/
-    cp ./temp_prefix/lib/libmlir_c_runner_utils.*.* $PREFIX/lib/
     cp ./temp_prefix/lib/libmlir_async_runtime.*.* $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_c_runner_utils.*.* $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_float16_utils.*.* $PREFIX/lib/
+    cp ./temp_prefix/lib/libmlir_runner_utils.*.* $PREFIX/lib/
 fi
 
 # clean up between builds
